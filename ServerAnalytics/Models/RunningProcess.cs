78196a87e80565a7ac828d045890dc6bf3831e7f
RxGroup <voronok309@mail.ru>
2023-02-20 19:05:18 +0700

    IIII

--- ServerAnalytics/Models/RunningProcess.cs
+++ ServerAnalytics/Models/RunningProcess.cs
@@ -1,12 +1,19 @@
-﻿namespace ServerAnalytics.Models
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace ServerAnalytics.Models
 {
     public class RunningProcess
     {
         public string Id { get; set; }
+        [Index(0)]
         public string Name { get; set; }
+        [Index(1)]
         public int PID { get; set; }
+        [Index(2)]
         public string NameSession { get; set; }
-        public int SessionNumber { get; set; } 
+        [Index(3)]
+        public int SessionNumber { get; set; }
+        [Index(4)]
         public string Memory { get; set; }
         public DateTimeOffset DateChek { get; set; }
     }
